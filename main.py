3af61e96048bebd6e4d173bf743bb9223e63004b
v.rieznikova <v.rieznikova@nodeart.io>
2024-05-01 09:44:36 +0300

    all functions

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import torch
 import time
 from utils import *
 from model import *
+from sklearn.metrics import confusion_matrix
     
 if __name__ == "__main__":
     torch.manual_seed(42)
     train_loader, eval_train_loader, eval_test_loader = CIFAR10_loaders()
-
     net = FFNet([3072, 3072, 3072, 10])
 
     time_training_start = time.time()
@@ -19,3 +19,84 @@ if __name__ == "__main__":
     print('train error:', str(round((1.0 - net.predict(eval_train_loader)) * 100, 2)) + '%')
 
     print('test error:', str(round((1.0 - net.predict(eval_test_loader)) * 100, 2)) + '%')
+
+    def iterate_cm(dims, num_epochs, pow = 4, norm = 3, cm = None, model_path = None):
+        net = FFNet(dims, pow, norm)  
+        if(model_path):
+          net.load_state_dict(torch.load(model_path))
+        time_training_start = time.time()
+        net.train(train_loader, num_epochs, cm)
+        time_training_end = time.time()
+        training_time = time_training_end - time_training_start
+        train_accuracy, predicted_y_train, y_train = net.predict(eval_train_loader)
+        test_accuracy, predicted_y_test, y_test = net.predict(eval_test_loader)
+        cm = confusion_matrix(y_test.tolist(), predicted_y_test.tolist())
+
+        # print("Total parameters:", net.count_neurons())
+        return round(training_time, 2), round(train_accuracy * 100, 2), round(test_accuracy * 100, 2), cm, net
+
+    def cm_experiment(
+        dims, 
+        num_epochs = 60, 
+        iters = 3, 
+        pow = 2, 
+        norm = 2, 
+        model_paths = [], 
+        cm=[], 
+        times=[], 
+        train_accuracies=[],
+        test_accuracies=[]
+        ):
+      model_path = None  
+      for i in range(1, iters + 1):
+        print("Epochs:",(i-1)*num_epochs, "-", i*num_epochs)
+        if(len(cm)): 
+          cm = clarify_cm(cm)
+        if(len(model_paths)): 
+          model_path = model_paths[-1]
+
+        training_time, train_accuracy, test_accuracy, cm, net = iterate_cm(dims, num_epochs, pow, norm, cm, model_path)
+        if(type(cm) == 'list'):
+          cm = np.array(cm)
+
+        file_path = f"modelname_{i}.pth"
+        torch.save(net.state_dict(), file_path)
+        model_paths.append(file_path)
+
+        times.append(time)
+        train_accuracies.append(time)
+        test_accuracies.append(time)
+
+        plt_cm(cm)
+        print("Time", training_time, "s")
+        print("Train accuracy", train_accuracy, "%")
+        print("Test accuracy", test_accuracy, "%")
+      return model_paths, train_accuracies, test_accuracies, times, cm
+    
+    cm_experiment(dims=[3072, 3072, 3072], num_epochs = 10, iters = 6)
+
+    def experiment(dims, epochs = 90, steps=15, pow = 2, norm = 2, iters = 3):
+        times = []
+        train_accuracies = []
+        test_accuracies = []
+
+        # stages = epochs / steps
+        for i in range(iters):
+            net = FFNet(dims, pow, norm)  
+            time_training_start = time.time()
+            net.train(train_loader, epochs, cm=[])
+            time_training_end = time.time()
+            training_time = time_training_end - time_training_start
+            times.append(training_time)
+            train_accuracy, predicted_y_train, y_train = net.predict(eval_train_loader)
+            test_accuracy, predicted_y_test, y_test = net.predict(eval_test_loader)
+            train_accuracies.append(train_accuracy * 100)
+            test_accuracies.append(test_accuracy * 100)
+
+        print("Total parameters:", net.count_neurons())
+        meanWithStdDeviation("Time", times, "s")
+        meanWithStdDeviation("Train accuracy", train_accuracies, "%")
+        meanWithStdDeviation("Test accuracy", test_accuracies, "%")
+
+        plt_cm(confusion_matrix(y_test.tolist(), predicted_y_test.tolist()))
+    experiment(dims=[3072, 3072, 3072], epochs = 20, iters = 3)
--- utils.py
+++ utils.py
@@ -1,9 +1,38 @@
 import torch
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import MNIST, CIFAR10
 from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+from mlxtend.plotting import plot_confusion_matrix
+
+def MNIST_loaders(train_batch_size=1000, test_batch_size=10000):
+
+    transform = Compose([
+        ToTensor(),
+        Normalize((0.1307,), (0.3081,)),
+        Lambda(lambda x: torch.flatten(x))])
+
+    train_loader = DataLoader(
+        MNIST('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=train_batch_size, shuffle=True)
+
+    eval_train_loader = DataLoader(
+        MNIST('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=test_batch_size, shuffle=False)
+
+    eval_test_loader = DataLoader(
+        MNIST('./data/', train=False,
+              download=True,
+              transform=transform),
+        batch_size=test_batch_size, shuffle=False)
+
+    return train_loader, eval_train_loader, eval_test_loader
 
 def CIFAR10_loaders(train_batch_size=1000, test_batch_size=10000):
 
@@ -32,6 +61,85 @@ def CIFAR10_loaders(train_batch_size=1000, test_batch_size=10000):
 
     return train_loader, eval_train_loader, eval_test_loader
 
+class ImbalanceCIFAR10(CIFAR10):
+    cls_num = 10
+
+    def __init__(self, root, imb_type='exp', imb_factor=0.01, rand_number=1, train=True,
+                 transform=None, target_transform=None, download=False):
+        super(ImbalanceCIFAR10, self).__init__(root, train, transform, target_transform, download)
+        np.random.seed(rand_number)
+        img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
+        self.gen_imbalanced_data(img_num_list)
+
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        elif imb_type == 'step':
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max))
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max * imb_factor))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+        # np.random.shuffle(classes)
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+        
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
+
+def CIFAR10_imbalanced_loaders(train_batch_size=1000, test_batch_size=10000):
+
+    transform = Compose([
+        ToTensor(),
+        Normalize((0.1307,), (0.3081,)),
+        Lambda(lambda x: torch.flatten(x))])
+
+    train_loader = DataLoader(
+        ImbalanceCIFAR10('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=train_batch_size, shuffle=True)
+
+    eval_train_loader = DataLoader(
+        CIFAR10('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=test_batch_size, shuffle=False)
+
+    eval_test_loader = DataLoader(
+        CIFAR10('./data/', train=False,
+              download=True,
+              transform=transform),
+        batch_size=test_batch_size, shuffle=False)
+
+    return train_loader, eval_train_loader, eval_test_loader
+
+train_loader, eval_train_loader, eval_test_loader = CIFAR10_imbalanced_loaders()
+
 def create_data_pos(images, labels):
     return overlay_labels_on_images(images, labels)
 
@@ -53,8 +161,36 @@ def overlay_labels_on_images(images, labels):
     return data
 
 def visualize_sample(data, name='', idx=0):
-    reshaped = data[idx].cpu().reshape(32, 32)
-    # plt.figure(figsize = (4, 4))
-    # plt.title(name)
-    # plt.imshow(reshaped, cmap="gray")
-    # plt.show()
+    reshaped = data[idx].cpu().reshape(28, 28)
+    plt.figure(figsize = (4, 4))
+    plt.title(name)
+    plt.imshow(reshaped, cmap="rgb")
+    plt.show()
+
+def meanWithStdDeviation(name, lst, unit):
+    values = torch.tensor(lst)
+    mean = round(torch.mean(values).item(), 2)
+    std = round(torch.std(values).item(), 2)
+    print(name, ": ", mean, "±", std, unit)
+
+def clarify_cm(confusion_matrix):
+    num_classes = confusion_matrix.shape[0]
+    mean = np.mean(confusion_matrix, axis=None)
+    for i in range(num_classes):
+        for j in range(num_classes):
+            if i == j:
+                confusion_matrix[i][j] = 0
+            else:
+              if confusion_matrix[i][j] < mean:
+                confusion_matrix[i][j] += random.randint(1, int(mean/3))
+              else:
+                confusion_matrix[i][j] = mean/3
+
+    return confusion_matrix
+
+def plt_cm(cm):
+  plot_confusion_matrix(conf_mat=cm, figsize=(8,8))
+  plt.title('Confusion Matrix', fontsize=14)
+  plt.tight_layout()
+  plt.show()
+
